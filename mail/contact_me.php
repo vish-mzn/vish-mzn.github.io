db2bb5aaaecbcbd17aa77673e374d76c857d3a03
Vishesh Pahuja <vishesh.pahuja@cognizant.com>
2019-09-16 19:02:47 +0530

    mail Id updated

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -14,7 +14,7 @@ $email_address = $_POST['email'];
 $message = $_POST['message'];
 	
 // Create the email and send the message
-$to = 'nemish452@gmail.com';
+$to = 'vish.ditu@gmail.com';
 
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
